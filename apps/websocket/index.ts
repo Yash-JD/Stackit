57fb996e3f5add824102274902122d18d006a3b6
Yash <yashdatwork@gmail.com>
2026-08-30 21:13:44 +0530

    Implement WebSocket server for board user management and notifications

--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -1,4 +1,70 @@
-import { WebSocketServer } from "ws";
-import { prisma } from "db/client";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer();
+const wss = new WebSocketServer({ port: 3002 });
+
+const BOARDS: Record<string, { userId: string; socket: WebSocket }[]> = {};
+
+wss.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    const parseData = JSON.parse(data.toString());
+
+    // TODO: handle jwt to identify userId
+
+    if (parseData.type === "join") {
+      const boardId = parseData.boardId;
+
+      // if boardId does not exist in BOARDS, create an array for it and add the user
+      if (!BOARDS[boardId]) {
+        BOARDS[boardId] = [{ userId: parseData.userId, socket: socket }];
+      }
+
+      // if boardId exists, check if userId already exists in the array
+      const userExists = BOARDS[boardId].some(
+        (user) => user.userId === parseData.userId,
+      );
+
+      // if userId does not exist, add the user to the array
+      if (!userExists) {
+        BOARDS[boardId].push({ userId: parseData.userId, socket: socket });
+      }
+
+      // send a message to all users in the board that a new user has joined
+      // TODO: also send userid, name, profile picture from db
+      // TODO: handle it in frontend
+      BOARDS[boardId]
+        .filter((user) => user.userId !== parseData.userId)
+        .forEach(({ socket }) => {
+          socket.send(
+            JSON.stringify({ type: "join", userId: parseData.userId }),
+          );
+        });
+
+      // send this user the list of all users in the board
+      const usersInBoard = BOARDS[boardId]
+        .filter((user) => user.userId !== parseData.userId)
+        .map((user) => user.userId);
+      socket.send(
+        JSON.stringify({ type: "initial_state", users: usersInBoard }),
+      );
+    }
+  });
+
+  socket.on("close", () => {
+    // Check if the user is in the board before removing
+    Object.entries(BOARDS).map(([boardId, users]) => {
+      const isUserInBoard = users.find((user) => user.socket == socket);
+      if (isUserInBoard) {
+        // Remove the user from the board
+        const remainingUsers = users.filter((user) => user.socket !== socket);
+        BOARDS[boardId] = remainingUsers;
+
+        // Notify remaining users in the board that a user has left
+        remainingUsers.forEach(({ socket }) => {
+          socket.send(
+            JSON.stringify({ type: "leave", userId: isUserInBoard.userId }),
+          );
+        });
+      }
+    });
+  });
+});
