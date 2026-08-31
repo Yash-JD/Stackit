e2703b120dd9cf989b98b35abf7225c163df6263
Yash <yashdatwork@gmail.com>
2026-08-31 08:15:00 +0530

    Refactor user ID to handle dummy userid for now

--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -13,35 +13,34 @@ wss.on("connection", (socket) => {
     if (parseData.type === "join") {
       const boardId = parseData.boardId;
 
+      // create dummy userId for now, in the future we will get it from jwt
+      const userId = parseData.userId || `user_${Date.now()}`;
+
       // if boardId does not exist in BOARDS, create an array for it and add the user
       if (!BOARDS[boardId]) {
-        BOARDS[boardId] = [{ userId: parseData.userId, socket: socket }];
+        BOARDS[boardId] = [{ userId, socket: socket }];
       }
 
       // if boardId exists, check if userId already exists in the array
-      const userExists = BOARDS[boardId].some(
-        (user) => user.userId === parseData.userId,
-      );
+      const userExists = BOARDS[boardId].some((user) => user.userId === userId);
 
       // if userId does not exist, add the user to the array
       if (!userExists) {
-        BOARDS[boardId].push({ userId: parseData.userId, socket: socket });
+        BOARDS[boardId].push({ userId, socket: socket });
       }
 
       // send a message to all users in the board that a new user has joined
       // TODO: also send userid, name, profile picture from db
       // TODO: handle it in frontend
       BOARDS[boardId]
-        .filter((user) => user.userId !== parseData.userId)
+        .filter((user) => user.userId !== userId)
         .forEach(({ socket }) => {
-          socket.send(
-            JSON.stringify({ type: "join", userId: parseData.userId }),
-          );
+          socket.send(JSON.stringify({ type: "join", userId }));
         });
 
       // send this user the list of all users in the board
       const usersInBoard = BOARDS[boardId]
-        .filter((user) => user.userId !== parseData.userId)
+        .filter((user) => user.userId !== userId)
         .map((user) => user.userId);
       socket.send(
         JSON.stringify({ type: "initial_state", users: usersInBoard }),
